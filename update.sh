5acb145bf2fbf5918d3438de34b923407760f252
GenDeathrow <kennywd.lmt@gmail.com>
2015-05-20 00:22:05 -0500

    update

--- update.sh
+++ update.sh
@@ -13,32 +13,34 @@ cd ./wiki
 git clone git@github.com:GenDeathrow/Skills.wiki.git ./
 git ls-files
 
+ls
+
 #Get Current Version from Github
-curversion=$(grep -oP "<${build_text}>(.*)</${build_text}>" Version_Info.md| cut -d '>' -f 2 | cut -d '<' -f 1)
+#curversion=$(grep -oP "<${build_text}>(.*)</${build_text}>" Version_Info.md | cut -d '>' -f 2 | cut -d '<' -f 1)
 
 cd ../
 
 # Replace with update version in config.xml
-grep -lR -e "<version>.*<\/version>" config.xml | xargs sed -i "s/<version>.*<\/version>/<version>${curversion}<\/version>/g"
+####################grep -lR -e "<version>.*<\/version>" config.xml | xargs sed -i "s/<version>.*<\/version>/<version>${curversion}<\/version>/g"
 
 # Increment Version Number 
-eval ./gradlew -q ${buildtype}
+########################eval ./gradlew -q ${buildtype}
 
 #Get new Version number
-newversion=$(grep -oP "<version>(.*)</version>" config.xml | cut -d '>' -f 2 | cut -d '<' -f 1)
+###################################newversion=$(grep -oP "<version>(.*)</version>" config.xml | cut -d '>' -f 2 | cut -d '<' -f 1)
 
 #Replace all ver keys with new Number
-grep -lRr -e $ver_key * | xargs sed -i "s/$ver_key/$newversion/g"
+###########################################grep -lRr -e $ver_key * | xargs sed -i "s/$ver_key/$newversion/g"
 
 #Build Forge
-./gradlew clean setupCIWorkSpace build
+###############################./gradlew clean setupCIWorkSpace build
 
 
 #Move back to wiki to update
 cd ./wiki
 
 # Replace with update version in  wiki
-grep -lR -e "<${build_text}>.*<\/${build_text}>" *| xargs sed -i "s/<${build_text}>.*<\/${build_text}>/<${build_text}>${newversion}<\/${build_text}>/g"
+##################################grep -lR -e "<${build_text}>.*<\/${build_text}>" *| xargs sed -i "s/<${build_text}>.*<\/${build_text}>/<${build_text}>${newversion}<\/${build_text}>/g"
 
 
 #Replace old build with new one
