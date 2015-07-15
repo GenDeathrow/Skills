703109efea636430657f41f443a67d20ad0c71b8
GenDeathrow <kennywd.lmt@gmail.com>
2015-07-14 23:46:19 -0500

    removed some code

--- src/main/java/com/gendeathrow/skills/client/gui/GuiManager.java
+++ src/main/java/com/gendeathrow/skills/client/gui/GuiManager.java
@@ -34,16 +34,9 @@ public class GuiManager
 		}
 		
 		GL11.glPushMatrix();
-			GL11.glDisable(GL11.GL_LIGHTING);
-	        //GL11.glEnable(GL11.GL_BLEND);
-	        GL11.glDisable(GL11.GL_TEXTURE_2D);
-	        //GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
-			GL11.glColor4f(1F, 1F, 1F, 1F);
 				DebugHud.instance.DrawDebugHud(event);
-			GL11.glEnable(GL11.GL_LIGHTING);
-	        GL11.glEnable(GL11.GL_TEXTURE_2D);
-	      //  GL11.glDisable(GL11.GL_BLEND);
 		GL11.glPopMatrix();
+		
 	}
 
 	
